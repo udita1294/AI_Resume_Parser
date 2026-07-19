e98c6414a8c206b22dca67bf3a4f0971e383c120
Udita Singh <udita1294@gmail.com>
2026-07-19 17:42:57 +0530

    Ai invoke

--- resume_parser.py
+++ resume_parser.py
@@ -1,1 +1,15 @@
-  
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+from pydantic import BaseModel, Field
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY not found.")
+
+client = Groq(api_key=my_api_key)
+model = "llama-3.3-70b-versatile"
+
+
